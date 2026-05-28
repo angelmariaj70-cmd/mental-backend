4f7d52dfd1b52989049a29dc2dd54ae88f74beb3
angelmariaj70-cmd <angelmariaj70@gmail.com>
2026-05-28 21:39:09 +0530

    small  backend changes

--- index.js
+++ index.js
@@ -21,13 +21,21 @@ const { Server } = require("socket.io");
 
 const server = http.createServer(mentalServer);
 
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST"]
+//   }
+// });
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://mental-frontend-five.vercel.app"
+    ],
     methods: ["GET", "POST"]
   }
 });
-
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
